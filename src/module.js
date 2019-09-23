dc219b0bed3986761eb207e4f4bb0c008ed725e7
Ari Wibowo <arbo77@gmail.com>
2019-09-23 08:50:29 +0700

    add angular-filter

--- src/module.js
+++ src/module.js
@@ -1,13 +1,13 @@
 import angular from 'angular'
 import 'angular-sanitize'
+import 'angular-filter'
 import 'angular-loading-bar'
 import 'angular-loading-bar/build/loading-bar.min.css'
-import Dexie from 'dexie'
-import 'dexie-observable'
 
 const module = angular.module('app', [
   'ngSanitize',
   'angular-loading-bar',
+  'angular.filter',
 ])
 
 export default module
